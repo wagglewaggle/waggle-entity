479745da8d67e6306477eaa2625dedce70aefb95
jiho5993 <jiho5993@naver.com>
2023-04-29 22:46:00 +0900

    initial commit

---
+++ src/skt-population/skt-population.constant.ts
@@ -0,0 +1,7 @@
+export enum SktPopulationLevel {
+  VeryRelaxation = 'VERY_RELAXATION',
+  Relaxation = 'RELAXATION',
+  Normal = 'NORMAL',
+  Crowded = 'CROWDED',
+  VeryCrowded = 'VERY_CROWDED',
+}
